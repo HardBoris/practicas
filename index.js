86d041229a181be899ac2b8603b8ed5864254c3e
HardBoris <bgaibor03@gmail.com>
2022-11-22 15:39:20 -0400

    feat: 4to desafio ok

--- index.js
+++ index.js
@@ -92,3 +92,97 @@ const conversor = (degree) => {
 /* console.log(conversor("25f"))
 console.log(conversor("32C"))
 console.log(conversor("45Y")) */
+
+/* 
+Buscando e contando dados em um array
+
+Baseado no Array de Livros por categorias abaixo, faça os seguintes desafios:
+
+    * Contar o número de categorias e o número de livros de cada categoria
+    * Contar o número de autores
+    * Mostrar livros do autor Augusto Cury
+    * Transformar a função acima em uma função que irá receber o nome do autor
+      e devolver os livros desse autor.
+*/
+
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da Narco milionária",
+                author: "T. Mark E"
+            },
+            {
+                title: "O homem mais rico da babilônia",
+                author: "George"
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert"
+            }
+        ]
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é casualidade",
+                author: "Augusto Cury"
+            },
+            {
+                title: "Ansiedade = Como enfrentar o rei do século",
+                author: "Augusto Cury"
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen"
+            }
+        ]
+    }
+]
+
+const categoriasCounter = booksByCategory.length
+let autors = []
+const libros = () => {
+    let qty = 0
+    for (let i = 0; i < booksByCategory.length; i++) {
+        const element = booksByCategory[i];
+        qty = element.books.length
+        return qty += qty
+    }
+}
+
+const autores = () => {
+    let qty = 0
+    for (let i = 0; i < booksByCategory.length; i++) {
+        const element = booksByCategory[i];
+        for (let j = 0; j < element.books.length; j++) {
+            const ele = element.books[j];
+            if (!autors.includes(ele.author)) {
+                autors.push(ele.author)
+            }
+        }
+        
+    }
+    return qty = autors.length
+}
+
+const librosPorAutor = (autor) => {
+    let libros = []
+    for (let i = 0; i < booksByCategory.length; i++) {
+        const element = booksByCategory[i];
+        for (let j = 0; j < element.books.length; j++) {
+            const ele = element.books[j];
+            if (autor === ele.author) {
+                libros.push(ele.title)
+            }
+        }
+    }
+    return libros
+}
+console.log("categorias: " + categoriasCounter)
+console.log("libros: " + libros())
+console.log("autores: " + autores())
+console.log(librosPorAutor("Stephen"))
+console.log(librosPorAutor("Augusto Cury"))
